1b474d81c94d55b1f220602a088fe985268c742a
Aurel Ioan Patrutiu <aurelioan93@gmail.com>
2022-03-27 19:07:59 +0200

    Move credentials to env config and add Master config to heroku

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,9 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-        publishable_token: Rails.application.credentials.iex_client.dig(:sandbox, :publishable_token),
-        secret_token: Rails.application.credentials.iex_client.dig(:sandbox, :secret_token),
-        endpoint: Rails.application.credentials.iex_client.dig(:sandbox, :endpoint)
+        publishable_token: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :publishable_token),
+        secret_token: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :secret_token),
+        endpoint: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :endpoint)
     )
     client.price(ticker_symbol)
   end
