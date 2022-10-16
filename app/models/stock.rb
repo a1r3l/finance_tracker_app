5578fcac3e7dea65d87df1905dc3c6952237be6b
Aurel Ioan Patrutiu <aurelioan93@gmail.com>
2022-10-16 20:51:56 +0200

    Add UserStock has many relationship

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,14 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-        publishable_token: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :publishable_token),
-        secret_token: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :secret_token),
-        endpoint: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :endpoint)
+      publishable_token: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :publishable_token),
+      secret_token: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :secret_token),
+      endpoint: Rails.application.credentials[Rails.env.to_sym].dig(:iex_client, :endpoint)
     )
 
     begin
